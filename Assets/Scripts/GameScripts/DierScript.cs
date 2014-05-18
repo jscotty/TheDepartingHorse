acebbaf53ef0abddc2a4f6ed850e0b1d3253c340
Alex Onbekend <ViridielLorael@gmail.com>
2014-05-18 15:08:19 +0200

    DierScript / Obstakel

---
+++ Assets/Scripts/GameScripts/DierScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DierScript : ObjectScript {
+	void Update () {
+		if (transform.position.x == 13f) 
+		{
+			transform.position.x -= 0.01f;
+		}
+		if (transform.position.x == 6f) 
+		{
+			transform.position.x += 0.01f;
+		}
+	}
+}
