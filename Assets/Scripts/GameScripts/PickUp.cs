5c84d8164020b25c27f48acfadb589a0c406c44f
Alex Onbekend <ViridielLorael@gmail.com>
2014-05-19 15:43:54 +0200

    Pickup Class & Flying Pick Up Class

---
+++ Assets/Scripts/GameScripts/PickUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+ 	void OnCollisionEnter2D(Collision2D player)
+	{
+		if (player.transform.tag == "Player")
+		{
+			Destroy(gameObject);
+			PickThePickUp();
+		}
+	}
+
+	virtual protected void PickThePickUp()
+	{
+		print("Picked Up!");
+	}
+}
