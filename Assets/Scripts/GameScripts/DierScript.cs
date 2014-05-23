dddd5118a4ac1d388f79c4c104935bd915a23f54
Alex Onbekend <ViridielLorael@gmail.com>
2014-05-23 17:07:19 +0200

    Animal Spawn fix. > Het zit nu op de camera ipv het dier.

--- Assets/Scripts/GameScripts/DierScript.cs
+++ Assets/Scripts/GameScripts/DierScript.cs
@@ -2,29 +2,13 @@
 using System.Collections;
 
 public class DierScript : MonoBehaviour {
-	public Transform animal;
 
-	private float timer = 0;
-
-
-	void Update () {
-		timer += Time.deltaTime;
-		if (timer >= 10) {
-			SpawnAnimal ();
-			timer = 0;
-		}
-
+	void Update(){
 		if (transform.position.x <= -43) {
 			Destroy(gameObject);
 		}
-
 	}
 
-	void SpawnAnimal(){
-		Vector3 playerLocation = new Vector3 (30, 0, 0);
-		Instantiate (animal, playerLocation, transform.rotation);
-	}
-	
 	void OnCollisionEnter2D(Collision2D player)
 	{
 		if (player.transform.tag == "Player" && PlayerController.catching == false)
---
+++ Assets/SpawnAnimal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAnimal : MonoBehaviour {
+	public Transform animal;
+	public float spawnTimer = 10;
+	
+	private float timer = 0;
+	
+	
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= spawnTimer) {
+			SpawnTheAnimal();
+			timer = 0;
+		}
+	}
+
+	void SpawnTheAnimal(){
+		Vector3 playerLocation = new Vector3 (30, 0, 0);
+		Instantiate (animal, playerLocation, transform.rotation);
+	}
+}
