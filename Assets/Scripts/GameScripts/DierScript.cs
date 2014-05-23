f64f8a2749323ee998cb8c29cb69c5b7a71246c3
Alex Onbekend <ViridielLorael@gmail.com>
2014-05-23 15:51:24 +0200

    Dier script: Verwijderd dat hij heen en weer gaat. Toegevoegt: Als de locatie <= x is, dan verwijderd hij zichzelf.

--- Assets/Scripts/GameScripts/DierScript.cs
+++ Assets/Scripts/GameScripts/DierScript.cs
@@ -4,30 +4,18 @@ using System.Collections;
 public class DierScript : ObjectScript {
 	public Transform animal;
 
-	public float movementSpeed;
-
-	private int counter = 0;
 	private float timer = 0;
 
-	private bool goToRight = false;
 
 	void Update () {
-				if (counter == 0) {
-						goToRight = true;
-				} else if (counter == 101) {
-						goToRight = false;
-				}
-				if (goToRight == true) {
-						transform.Translate (Vector2.right * movementSpeed);
-						counter++;
-				} else if (goToRight == false) {
-						transform.Translate (Vector2.right * -movementSpeed);
-						counter--;
-				}
 		timer += Time.deltaTime;
 		if (timer >= 10) {
 			SpawnAnimal ();
 			timer = 0;
+		}
+
+		if (transform.position.x <= -43) {
+			Destroy(gameObject);
 		}
 
 	}
